fc9d9ac439ee9d82eba3f9d466044337b72af4e3
MikeNelli <themikenel@gmail.com>
2024-02-09 09:20:55 +0500

    BankAccounts
    IDK

---
+++ ViewModels/ViewModelAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BankASystem.ViewModels
+{
+    public class ViewModelAccount : INotifyPropertyChanged
+    {
+        public
+        public UIElement Visibility
+        {
+            get => 
+        }
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
